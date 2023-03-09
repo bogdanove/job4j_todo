2a1cda2b908ae77a755319cf9028b75f5b37b74b
bogdanove <anoth3rx@gmail.com>
2023-03-09 20:03:57 +0300

    6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/todo/repository/TaskStore.java
+++ src/main/java/ru/job4j/todo/repository/TaskStore.java
@@ -1,142 +1,54 @@
 package ru.job4j.todo.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class TaskStore {
 
-    private final SessionFactory sf;
+    private final CrudRepository repository;
 
     public List<Task> findAll() {
-        Session session = sf.openSession();
-        List tasks = new ArrayList();
-        try {
-            session.beginTransaction();
-            tasks = session.createQuery(
-                    "from Task").list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return tasks;
+        return repository.query("from Task", Task.class);
     }
 
     public List<Task> findAllDone() {
-        Session session = sf.openSession();
-        List tasks = new ArrayList();
-        try {
-            session.beginTransaction();
-            tasks = session.createQuery(
-                    "from Task where done = true").list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return tasks;
+        return repository.query("from Task where done = true", Task.class);
     }
 
     public List<Task> findAllNew() {
-        Session session = sf.openSession();
-        List tasks = new ArrayList();
-        try {
-            session.beginTransaction();
-            tasks = session.createQuery(
-                    "from Task where done = false").list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return tasks;
+        return repository.query("from Task where done = false", Task.class);
     }
 
     public Task add(Task task) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.save(task);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
+        repository.run(session -> session.persist(task));
         return task;
     }
 
     public Optional<Task> findById(int id) {
-        Session session = sf.openSession();
-        Optional task = Optional.empty();
-        try {
-            session.beginTransaction();
-            task = session.createQuery(
-                            "from Task WHERE id = :fId")
-                    .setParameter("fId", id).uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return task;
+        return repository.optional("from Task WHERE id = :fId", Task.class, Map.of("fId", id));
     }
 
     public boolean delete(int id) {
-        Session session = sf.openSession();
-        boolean result = false;
-        try {
-            session.beginTransaction();
-            int count = session.createQuery(
-                            "DELETE Task WHERE id = :fId")
-                    .setParameter("fId", id)
-                    .executeUpdate();
-            result = count > 0;
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return result;
+        return repository.run("DELETE Task WHERE id = :fId", Map.of("fId", id)) > 0;
     }
 
     public boolean replace(Task task) {
-        Session session = sf.openSession();
-        boolean result = false;
-        try {
-            session.beginTransaction();
-            int count = session.createQuery(
-                            "UPDATE Task SET name = :fName, description = :fDescription, done = :fDone WHERE id = :fId")
-                    .setParameter("fName", task.getName())
-                    .setParameter("fDescription", task.getDescription())
-                    .setParameter("fDone", task.isDone())
-                    .setParameter("fId", task.getId())
-                    .executeUpdate();
-            result = count > 0;
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return result;
+        return repository.run(
+                "UPDATE Task SET name = :fName, description = :fDescription, done = :fDone WHERE id = :fId",
+                Map.of("fName", task.getName(), "fDescription", task.getDescription(), "fDone",
+                        task.isDone(), "fId", task.getId())) > 0;
     }
 
     public boolean done(int id) {
-        Session session = sf.openSession();
-        boolean result = false;
-        try {
-            session.beginTransaction();
-            int count = session.createQuery(
-                            "UPDATE Task SET done = true WHERE id = :fId")
-                    .setParameter("fId", id)
-                    .executeUpdate();
-            result = count > 0;
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return result;
+        return repository.run("UPDATE Task SET done = true WHERE id = :fId",
+                Map.of("fId", id)) > 0;
     }
 }
 
--- src/main/java/ru/job4j/todo/repository/UserStore.java
+++ src/main/java/ru/job4j/todo/repository/UserStore.java
@@ -1,61 +1,36 @@
 package ru.job4j.todo.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class UserStore {
 
-    private final SessionFactory sf;
+    private final CrudRepository repository;
 
     public Optional<User> add(User u) {
-        Session session = sf.openSession();
-        Optional user = Optional.empty();
+        Optional<User> user = Optional.empty();
         try {
-            session.beginTransaction();
-            session.save(u);
-            session.getTransaction().commit();
+            repository.run(session -> session.persist(u));
             user = Optional.of(u);
         } catch (Exception e) {
-            session.getTransaction().rollback();
+            e.printStackTrace();
         }
         return user;
     }
 
     public Optional<User> findById(int id) {
-        Session session = sf.openSession();
-        Optional user = Optional.empty();
-        try {
-            session.beginTransaction();
-            user = session.createQuery(
-                            "from User WHERE id = :fId")
-                    .setParameter("fId", id).uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return user;
+        return repository.optional("from User WHERE id = :fId",
+                User.class, Map.of("fId", id));
     }
 
     public Optional<User> findUserByEmailAndPassword(String email, String password) {
-        Session session = sf.openSession();
-        Optional user = Optional.empty();
-        try {
-            session.beginTransaction();
-            user = session.createQuery(
-                            "from User WHERE email = :fEmail AND password = :fPassword")
-                    .setParameter("fEmail", email)
-                    .setParameter("fPassword", password).uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return user;
+        return repository.optional("from User WHERE email = :fEmail AND password = :fPassword",
+                User.class, Map.of("fEmail", email, "fPassword", password));
     }
 }
