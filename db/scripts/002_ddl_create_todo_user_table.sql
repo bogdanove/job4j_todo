0a9a6a6eda6162d3a52c1798254c14e44f5fc4f4
bogdanove <anoth3rx@gmail.com>
2023-03-10 20:48:00 +0300

    0. ToOne [#6873]

--- db/scripts/002_ddl_create_todo_user_table.sql
+++ db/scripts/002_ddl_create_todo_user_table.sql
@@ -7,5 +7,3 @@ create table if not exists todo_user
 );
 
 insert into todo_user (name, email, password) values ('Name', 'mail@mail.ru', 'qwerty');
-
-ALTER TABLE tasks ADD COLUMN user_id int not null references todo_user(id) default 1;
